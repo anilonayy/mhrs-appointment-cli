cd096663570439c82d7fbdf9e8960690ed2f10b9
Cengizhan Anıl ONAY <anil.onay@useinsider.com>
2024-10-28 01:31:31 +0300

    refactor: add user headers

--- pkg/resty/resty.go
+++ pkg/resty/resty.go
@@ -16,7 +16,20 @@ func GetClient() *resty.Client {
 		client = resty.New().
 			SetTimeout(5 * time.Second).
 			SetRetryCount(3).
-			SetRetryWaitTime(5 * time.Second)
+			SetRetryWaitTime(5 * time.Second).
+			SetHeaders(map[string]string{
+				"accept":                           "application/json, text/plain, */*",
+				"accept-language":                  "tr-TR",
+				"access-control-allow-credentials": "true",
+				"access-control-allow-headers":     "Authorization,Content-Type, Accept, X-Requested-With, remember-me",
+				"access-control-allow-methods":     "DELETE, POST, GET, OPTIONS",
+				"access-control-allow-origin":      "*",
+				"access-control-max-age":           "3600",
+				"content-type":                     "application/json",
+				"sec-ch-ua":                        "\"Chromium\";v=\"130\", \"Google Chrome\";v=\"130\", \"Not?A_Brand\";v=\"99\"",
+				"sec-ch-ua-mobile":                 "?0",
+				"sec-ch-ua-platform":               "\"macOS\"",
+			})
 	})
 
 	return client
